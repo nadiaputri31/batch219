c06017b6ce22e9262ecac578162437977f08e665
jevonandros <jevon017@gmail.com>
2019-10-24 10:42:15 +0700

    test

---
+++ src/com/xsisdayone/Practice1.java
@@ -0,0 +1,8 @@
+package com.xsisdayone;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Xsis Academy");
+        System.out.println("Hello \nAcademy");
+    }
+}
